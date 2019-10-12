17380665475c9b3f4d568885185ad5605d876622
javierfernandez <javier.fernandez.salgado@esa.int>
2019-10-12 21:22:41 +0200

    Initial commit

---
+++ Simulation.py
@@ -0,0 +1,102 @@
+import random
+import agent
+import matplotlib.pyplot as plt
+
+class Simulation(object):
+
+    def __init__(self, priceProtection, priceInsurance, initialProtabilitySteal, averageUtility, numberAgents ):
+        self.agents = []
+        self.priceProtection = priceProtection
+        self.priceInsurance = priceInsurance
+        self.initialProbabilitySteal = initialProtabilitySteal
+        self.averageUtility = averageUtility
+
+        self.totalCountInsurance = []
+        self.totalCountProtected = []
+        self.totalPriceInsurenace = []
+        for i in range(numberAgents):
+            self.agents.append(agent.Agent(float(random.uniform(0,100)/100), 0.50, random.randint(0,1), random.randint(0,1), random.uniform(0,1), random.uniform(0,1), random.uniform(0,1), random.uniform(0,1)))
+
+    def simulation (self,numberPeriods):
+        for i in range(numberPeriods):
+            self.period()
+        self.showChart(numberPeriods)
+    def period(self):
+
+        self.globalUpdate()
+        self.agentUpdate()
+        self.calculateAgents()
+
+
+    def globalUpdate(self):
+        if self.initialProbabilitySteal < 0.5:
+            self.initialProbabilitySteal = random.uniform(0, self.initialProbabilitySteal * 0.2)
+        else:
+            self.initialProbabilitySteal = random.uniform(self.initialProbabilitySteal * 2, 1.1)
+
+    def agentUpdate(self):
+
+        for i in self.agents:
+            self.selectFriend(i)
+        for i in self.agents:
+            i.updateUtility(self.priceProtection, self.priceInsurance, self.averageUtility)
+        for i in self.agents:
+            i.updateStealing(self.initialProbabilitySteal)
+        for i in self.agents:
+            i.updateLevelRichness(self.priceProtection, self.priceInsurance)
+
+        counterAgentsInsurance = 0
+        counterAgentsInsuranceAndSteal = 0
+        for i in self.agents:
+            counterAgentsInsurance = counterAgentsInsurance + i.isInsurance
+            counterAgentsInsuranceAndSteal = counterAgentsInsuranceAndSteal + i.isInsurance * i.isSteal
+        self.priceInsurance = self.agents[0].lostSteal * (counterAgentsInsuranceAndSteal/counterAgentsInsurance)
+
+        totalRich = 0
+        for i in self.agents:
+            totalRich = totalRich + i.richLevel
+        self.averageUtility = totalRich / len (self.agents)
+
+    def selectFriend(self, agent):
+        isSelectedAFriend = True
+        while isSelectedAFriend:
+            friend = random.randint(0, len(self.agents) - 1)
+            if self.agents[friend] != agent:
+                agent.friend = self.agents[friend]
+                isSelectedAFriend = False
+
+    def calculateAgents(self):
+        auxTotalCountInsurance = 0
+        auxTotalCountProtected = 0
+        priceInsurance = 0
+        for i in self.agents:
+            auxTotalCountInsurance = auxTotalCountInsurance + i.isInsurance
+            auxTotalCountProtected = auxTotalCountProtected + i.isProtect
+        self.totalCountProtected.append( auxTotalCountProtected / len(self.agents))
+        self.totalCountInsurance.append( auxTotalCountInsurance / len(self.agents))
+        self.totalPriceInsurenace.append(self.priceInsurance)
+    def showChart(self,numberSimulation):
+        plt.plot(range(numberSimulation), self.totalPriceInsurenace, color='g')
+        plt.plot(range(numberSimulation), self.totalCountInsurance, color='red')
+        plt.plot(range(numberSimulation), self.totalCountProtected, color='blue')
+
+
+        plt.xlabel('Number simulation')
+        plt.ylabel('price')
+        plt.title('Insurance simulation')
+        plt.show()
+
+        csv = open('insurance.csv', 'w')
+        csv.write("totalcountprotected,totalCountInsurance,totalPriceInsurance\n")
+        for row in range(len(self.totalCountProtected)):
+            csv.write(str(self.totalCountProtected[row]) + "," + str(self.totalCountInsurance[row]) + "," + str(self.totalPriceInsurenace[row]) + "\n")
+        csv.close()
+if __name__ == '__main__':
+    priceProtection = 0.05
+    priceInsurance = 0.90
+    initialProtabilitySteal = 1
+    averageUtility = 0.80
+    numberAgents = 500
+    s = Simulation(priceProtection, priceInsurance, initialProtabilitySteal, averageUtility, numberAgents)
+    s.simulation(2000)
+
---
+++ agent.py
@@ -0,0 +1,92 @@
+
+import random
+
+
+class Agent(object):
+    def __init__(self, richLevel, lostSteal, isProtect, isInsurance, probabilitySteal, expectUtility, isSteal, levelRichFinal):
+        self.richLevel = richLevel
+        self.lostSteal = lostSteal
+        self.isProtect = isProtect
+        self.isInsurance =isInsurance
+        self.probabilitySteal = probabilitySteal
+        self.expectUtility = expectUtility
+        self.isSteal = isSteal
+        self.levelRichFinal = levelRichFinal
+        self.friend = None
+        strategy = random.randint(0,2)
+        if strategy == 0:
+            self.strategy = StrategyMax(self)
+        elif strategy == 1:
+            self.strategy = StrategyImitation(self)
+        else:
+            self.strategy = StrategyCorrect(self)
+
+    def updateUtility(self, cr, cs, au):
+        #self.richLevel - self.probabilitySteal * self.lostSteal * (1 - int(self.isInsurance)) - int(self.isProtect) * cr - int(self.isInsurance) * cs
+        self.expectUtility = self.strategy.usefulPercent(cr, cs, au)
+    def updateStealing(self,pa0):
+        value = random.uniform(100,0)
+        if value < pa0 and not self.isProtect:
+            self.isSteal = True
+        else:
+            self.isSteal = False
+    def updateLevelRichness(self,cr,cs):
+        self.richLevel = self.richLevel - self.lostSteal - self.isSteal * ( 1 - self.isInsurance) - self.isProtect * cr - self.isInsurance * cs
+
+
+
+
+class Strategy(object):
+    def __init__(self,parent):
+        self.parent = parent
+        pass
+    def updateUsefulPercent(self,cr,cs,au):
+        pass
+class StrategyMax(Strategy):
+    def __init__(self, parent):
+        super(StrategyMax, self).__init__(parent)
+
+    def usefulPercent(self,cr,cs,au):
+
+        ucase0 = self.parent.richLevel - self.parent.probabilitySteal * self.parent.lostSteal * (1 - 0) - 0 * cr - 0 * cs
+
+        ucase1 =  self.parent.richLevel - self.parent.probabilitySteal * self.parent.lostSteal * (1 - 1) - 0 * cr - 1 * cs
+
+        ucase2 =  self.parent.richLevel - self.parent.probabilitySteal * self.parent.lostSteal * (1 - 0) - 1 * cr - 0 * cs
+
+        return  max(ucase0,ucase1,ucase2)
+
+class StrategyImitation(Strategy):
+    def __init__(self, parent: Agent):
+        super(StrategyImitation, self).__init__(parent)
+
+    def usefulPercent(self, cr, cs,au):
+        if (self.parent.friend.richLevel > self.parent.richLevel):
+            self.parent.isProtect = self.parent.friend.isProtect
+            self.parent.isInsurance = self.parent.friend.isInsurance
+        self.parent.richLevel - self.parent.probabilitySteal * self.parent.lostSteal * (1 - self.parent.isInsurance) - self.parent.isProtect * cr - self.parent.isInsurance * cs
+
+
+class StrategyCorrect(Strategy):
+    def __init__(self, parent: Agent):
+        super(StrategyCorrect, self).__init__(parent)
+
+    def usefulPercent(self, cr, cs, au):
+        if (self.parent.expectUtility < au):
+            if self.parent.isInsurance and not self.parent.isProtect:
+                self.parent.isInsurance = False
+                self.parent.isProtect = True
+            elif not self.parent.isInsurance and self.parent.isProtect:
+                self.parent.isInsurance = True
+                self.parent.isProtect = False
+            elif not self.parent.isInsurance and not self.parent.isProtect:
+                coin = random.randint(0, 1)
+                if coin == 0:
+                    self.parent.isInsurance = False
+                    self.parent.isProtect = True
+                else:
+                    self.parent.isInsurance = True
+                    self.parent.isProtect = False
+        return self.parent.richLevel - self.parent.probabilitySteal * self.parent.lostSteal * (1 - self.parent.isInsurance) - self.parent.isProtect * cr - self.parent.isInsurance * cs
+
+
